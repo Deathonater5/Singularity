898141fb068cff442c0ede74654b17a33414f797
bryan <bshetty@sfu.ca>
2019-09-26 13:00:59 -0700

    added a modal for login data is able to be sent over a post route

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,15 @@ router.get("/", (req, res) => {
 	res.render("landing")
 })
 
+// Register routes
+// Show register form
+
+
+
+// Login routes
+router.post("/login", (req, res) => {
+	res.send(req.body)
+})
+
+// Export
 module.exports = router;
